6b82ef8a8fb8c3b73c3f52f165e0ad4416d177c7
sajir <abdelhadisaj@gmail.com>
2022-04-28 02:49:19 +0000

    mettre storys bar dynamique

--- src/Components/StoryBar_component/StoryBar.js
+++ src/Components/StoryBar_component/StoryBar.js
@@ -3,65 +3,54 @@ import './StoryBar.css'
 import Avatar from '@mui/material/Avatar';
 import profile from '../../images/profile.png';
 
-export class StoryBar extends Component {
+class StoryBar extends Component {
+    constructor(props){
+        super(props);
+        this.state={
+          storyList : []
+          }
+  }
+    componentDidMount(){
+        this.getStoryData();
+    }
+
+  getStoryData = () => {
+    let data =[
+        {
+            "username":"username1",
+            "storyimg":"../../images/profile.png"
+        },
+
+        {
+            "username":"username2",
+            "storyimg":"../../images/profile.png"
+        },
+
+        {
+            "username":"username3",
+            "storyimg":"../../images/profile.png"
+        }
+
+    ]
+
+    this.setState({storyList: data});
+
+  }
+         
   render() {
     return (
       <div>
         <div className='storybar_container'>
-            <div className='story'>
-                <Avatar className='story_photo' src={profile}/>
-                <div className='story_username'>usernameusername</div>
-            </div>
+            {
+                this.state.storyList.map((item,index)=>{
 
-            <div className='story'>
-                <Avatar className='story_photo' src={profile}/>
-                <div className='story_username'>usernameusername</div>
-            </div>
+                        <div className='story'>
+                            <Avatar className='story_photo' src={profile}/>
+                            <div className='story_username'>{item.username}</div>
+                        </div>
 
-            <div className='story'>
-                <Avatar className='story_photo' src={profile}/>
-                <div className='story_username'>usernameusername</div>
-            </div>
-
-            <div className='story'>
-                <Avatar className='story_photo' src={profile}/>
-                <div className='story_username'>usernameusername</div>
-            </div>
-
-            <div className='story'>
-                <Avatar className='story_photo' src={profile}/>
-                <div className='story_username'>usernameusername</div>
-            </div>
-
-            <div className='story'>
-                <Avatar className='story_photo' src={profile}/>
-                <div className='story_username'>usernameusername</div>
-            </div>
-
-            <div className='story'>
-                <Avatar className='story_photo' src={profile}/>
-                <div className='story_username'>usernameusername</div>
-            </div>
-
-            <div className='story'>
-                <Avatar className='story_photo' src={profile}/>
-                <div className='story_username'>usernameusername</div>
-            </div>
-
-            <div className='story'>
-                <Avatar className='story_photo' src={profile}/>
-                <div className='story_username'>usernameusername</div>
-            </div>
-
-            <div className='story'>
-                <Avatar className='story_photo' src={profile}/>
-                <div className='story_username'>usernameusername</div>
-            </div>
-
-            <div className='story'>
-                <Avatar className='story_photo' src={profile}/>
-                <div className='story_username'>usernameusername</div>
-            </div>
+                })
+            }
 
         </div>
       </div>
