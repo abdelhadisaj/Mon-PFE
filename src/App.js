97b02a5acc027235bea20a9408b8e3ff8797303d
Oussama Laarouchi <oussama.laarouchi@internationalits.com>
2022-05-15 11:18:32 +0100

    hi

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@
 import './App.css';
 import HomePage from './Components/Homepage/HomePage';
 import LoginPage from './Components/Login-page/LoginPage';
+import { useState } from 'react';
 
 function App() {
+  const [loggedIn, setLoggedIn] = useState(false);
+
   return (
     <div className="App">
-      {/* <LoginPage/> */}
-      <HomePage/>
+      {loggedIn ? <HomePage/> : <LoginPage/>}
     </div>
   );
 }
